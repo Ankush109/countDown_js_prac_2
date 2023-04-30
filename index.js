7e549ac016076a5ed396f02fec0fafe003d9a4ec
Ankush Banerjee <banerjeeankush184@gmail.com>
2023-04-30 10:22:56 +0530

    js prac

---
+++ index.js
@@ -0,0 +1,63 @@
+(function(){
+var hour =document.querySelector('.hour')
+var min =document.querySelector('.minute')
+var sec =document.querySelector(".sec")
+var startbtn =document.querySelector(".start")
+var stopbtn=document.querySelector(".stop")
+var resetbtn =document.querySelector(".reset")
+var countdownTimer =null;
+startbtn.addEventListener("click",()=>{
+    if(hour.value== 0 && min.value==0 && sec.value==0) return;
+    function setT(){
+        startbtn.style.display="none"
+             stopbtn.style.display="initial"
+             countdownTimer=setInterval(()=>{
+                timer();
+             },1000) 
+    }
+    setT();
+})
+function stopInt(state){
+    startbtn.innerHTML=state==="pause" ? "Continue" :"Start"
+    startbtn.style.display="initial";
+    stopbtn.style.display="none"
+    clearInterval(countdownTimer);
+}
+function timer(){
+    if(sec.value>60){
+        min.value++;
+        sec.value=parseInt(sec.value)-59;
+    }
+    if(min.value>60){
+        hour.value++;
+           min.value=parseInt(min.value)-60;
+    }
+    if(hour.value==0 && min.value==0 && sec.value==0){
+        hour.value="";
+        min.value="";
+        sec.value="";
+        stopInt();
+    }
+    else if(sec.value!=0){
+        sec.value=`${sec.value<=10 ? "0" :""}${sec.value-1}`
+    }
+    else if(min.value!=0 && sec.value==0){
+        sec.value=59;
+             min.value=`${min.value<=10 ? "0" :""}${min.value-1}`
+    } 
+ else if(hour.value!=0 && min.value==0){
+        min.value=60;
+             hour.value=`${hour.value<=10 ? "0" :""}${hour.value-1}`
+    } 
+
+}
+stopbtn.addEventListener("click",()=>{
+    stopInt("pause")
+})
+resetbtn.addEventListener("click",()=>{
+    hour.value="";
+    min.value=""
+    sec.value=""
+    stopInt();
+})
+})();
